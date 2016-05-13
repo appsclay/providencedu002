0f4378af89b9d123cbbb30c782f12421832f6d6d
Sumeet Masih <billu.pillu123@gmail.com>
2016-05-13 13:27:45 +0530

    add s3 credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,8 +41,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
 
-
-
+#no credentials
 
   
 end
